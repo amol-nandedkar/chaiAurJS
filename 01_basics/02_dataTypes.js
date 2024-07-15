1868ce26c957c24b35e705a5210a0491dbaee521
amol-nandedkar <117171808+amol-nandedkar@users.noreply.github.com>
2024-07-15 03:23:31 +0000

    learn data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat the code in this file as newer version.
+
+
+// alert("abc"); this not not be used in node. available in browser.
+
+console.log(3+3);
+
+let name = "Amol";
+let age = 39
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => ftandalone value (empty value), its a type
+// undefined
+// symbol => used for Uniqueess
+// object
+
+
+console.log(typeof 3); // number
+console.log(typeof name); // string
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined

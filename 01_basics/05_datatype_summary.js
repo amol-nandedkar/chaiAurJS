265c892efca977fcdc3cbd896fc6c93f17e94b58
amol-nandedkar <117171808+amol-nandedkar@users.noreply.github.com>
2024-07-15 04:04:44 +0000

    datatype summary

---
+++ 01_basics/05_datatype_summary.js
@@ -0,0 +1,9 @@
+// Primitive
+// 7 types : Number, String, Boolean, Null ,undefined, Symbol, BigInt
+
+let id = Symbol("123");
+let anotherId = Symbol("123");
+console.log(id === anotherId); // false
+
+// Reference
+// Array, Object, Function

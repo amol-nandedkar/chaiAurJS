15721750b46fbcb013056bc441fc2f31c4db00a5
amol-nandedkar <117171808+amol-nandedkar@users.noreply.github.com>
2024-07-29 03:02:52 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,15 @@
+// while loop
+
+let index = 0;
+while (index <=20) {
+    console.log(`Value of index in ${index}`);
+    index = index + 2;
+}
+
+
+// do while
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);

a43aa84e8a3d10d30c3c6aa19ecbf7df6e634302
amol-nandedkar <117171808+amol-nandedkar@users.noreply.github.com>
2024-07-29 02:54:36 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,17 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(element);
+}
+
+
+let myArr = ["Superman","spiderman","ironman"];
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+}
+
+
+// break and continue
